7437e142dc5239a00ca41227dcaef2607d4bda98
Nima Naderi <nima.er.84@gmail.com>
2023-05-23 22:25:45 +0330

    Added All The App Colors And Gradients !

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,74 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Main Colors
+  static const Color primary = Color(0xffE21221);
+  static const Color secondary = Color(0xffFFD300);
+
+  // Alert - Status Colors
+  static const Color success = Color(0xff4ADE80);
+  static const Color info = Color(0xff246BFD);
+  static const Color warning = Color(0xffFACC15);
+  static const Color error = Color(0xffF75555);
+  static const Color disabled = Color(0xffD8D8D8);
+  static const Color disabledButton = Color(0xffC1232F);
+
+  // Greyscale Colors
+  static const Color grey900 = Color(0xff121212);
+  static const Color grey800 = Color(0xff424242);
+  static const Color grey700 = Color(0xff616161);
+  static const Color grey600 = Color(0xff757575);
+  static const Color grey500 = Color(0xff9E9E9E);
+  static const Color grey400 = Color(0xffBDBDBD);
+  static const Color grey300 = Color(0xffE0E0E0);
+  static const Color grey200 = Color(0xffEEEEEE);
+  static const Color grey100 = Color(0xffF5F5F5);
+  static const Color grey50 = Color(0xffFAFAFA);
+
+  // Dark Colors
+  static const Color dark1 = Color(0xff181A20);
+  static const Color dark2 = Color(0xff1F222A);
+  static const Color dark3 = Color(0xff35383F);
+
+  // Other Colors
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff000000);
+  static const Color red = Color(0xffF44336);
+  static const Color pink = Color(0xffE91E63);
+  static const Color purple = Color(0xff9C27B0);
+  static const Color deepPurple = Color(0xff673AB7);
+  static const Color indigo = Color(0xff3F51B5);
+  static const Color blue = Color(0xff2196F3);
+  static const Color lightBlue = Color(0xff03A9F4);
+  static const Color cynan = Color(0xff00BCD4);
+  static const Color teal = Color(0xff009688);
+  static const Color green = Color(0xff4CAF50);
+  static const Color lightGreen = Color(0xff8BC34A);
+  static const Color lime = Color(0xffCDDC39);
+  static const Color yellow = Color(0xffFFEB3B);
+  static const Color amber = Color(0xffFFC107);
+  static const Color orange = Color(0xffFF9800);
+  static const Color deepOrange = Color(0xffFF5722);
+  static const Color brown = Color(0xff795548);
+  static const Color blueGray = Color(0xff607D8B);
+
+  // Background Colors
+  static const Color backgroundRed = Color(0xffFFF5F6);
+  static const Color backgroundPurple = Color(0xffF4ECFF);
+  static const Color backgroundBlue = Color(0xffF6FAFD);
+  static const Color backgroundGreen = Color(0xffF2FFFC);
+  static const Color backgroundOrange = Color(0xffFFF8ED);
+  static const Color backgroundPink = Color(0xffFFF5F5);
+  static const Color backgroundYellow = Color(0xffFFFEE0);
+
+  // Transparent Colors
+  static const Color redTransparent = Color(0xffE21221);
+  static const Color purpleTransparent = Color(0xff7210FF);
+  static const Color blueTransparent = Color(0xff335EF7);
+  static const Color orangeTransparent = Color(0xffFF9800);
+  static const Color yellowTransparent = Color(0xffFACC15);
+  static const Color greenTransparent = Color(0xff4CAF50);
+  static const Color cyanTransparent = Color(0xff00BCD4);
+}
---
+++ lib/config/theme/app_gradients.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class AppGradients {
+  Gradient redGradient = const LinearGradient(
+    colors: [
+      Color(0xffFF4451),
+      Color(0xffE21221),
+    ],
+    // begin: Alignment.bottomCenter,
+    // end: Alignment.centerRight,
+  );
+
+  Gradient yellowGradient = const LinearGradient(
+    colors: [
+      Color(0xffFFE580),
+      Color(0xffFACC15),
+    ],
+    // begin: Alignment.centerLeft,
+    // end: Alignment.centerRight,
+  );
+
+  Gradient purpleGradient = const LinearGradient(
+    colors: [
+      Color(0xff9953FF),
+      Color(0xff7210FF),
+    ],
+    // begin: Alignment.centerLeft,
+    // end: Alignment.centerRight,
+  );
+
+  Gradient blueGradient = const LinearGradient(
+    colors: [
+      Color(0xff5F82FF),
+      Color(0xff335EF7),
+    ],
+    // begin: Alignment.centerLeft,
+    // end: Alignment.centerRight,
+  );
+
+  Gradient greenGradient = const LinearGradient(
+    colors: [
+      Color(0xff35DEBC),
+      Color(0xff22BB9C),
+    ],
+    // begin: Alignment.centerLeft,
+    // end: Alignment.centerRight,
+  );
+
+  Gradient orangeGradient = const LinearGradient(
+    colors: [
+      Color(0xffFFAB38),
+      Color(0xffFB9400),
+    ],
+    // begin: Alignment.centerLeft,
+    // end: Alignment.centerRight,
+  );
+}
